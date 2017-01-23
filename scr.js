3112b501b5dce16f21eb6fc9336b27916b9133e0
NickoMas <aquinnm@gmail.com>
2017-01-23 15:33:00 +0300

    add_modified-files

--- scr.js
+++ scr.js
@@ -23,11 +23,11 @@ function c_submit({e,el,obj,xhr}) {
 	let inps = e.target.querySelectorAll('input[type=text]');
 
 	Array.prototype.forEach.call(inps,function(a){
-		if(!a.value) {
-			a.value = 'Enter valid data!';
-			throw new Error('Irregular data');
-		}
-		inps_obj[a.name] = a.value;
+      if(!a.value) {
+      	a.value = 'Enter valid data!';
+      	throw new Error('Irregular data');
+      }
+      inps_obj[a.name] = a.value;
 	});
 
 	obj.data = inps_obj;
@@ -35,8 +35,8 @@ function c_submit({e,el,obj,xhr}) {
 };
 
 function c_list({e,obj,xhr,form}) {
-
-	xhr.onreadystatechange = function(){
+	
+  xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4 || xhr.status == 200){
 			console.log(xhr);
 			console.log(c_list_parser(xhr.responseText))
@@ -47,25 +47,32 @@ function c_list({e,obj,xhr,form}) {
 };
 
 function c_list_parser(obj){
-
+	
 	obj = JSON.parse(obj);
 
-	var head = `<tr><th>№</th><th>_Date_</th><th>Record</th><th>Price</th><th>Runout</th></tr>`;
-	var body = '';
+	var head   = `<tr><th>№</th><th>_Date_</th><th>Record</th><th>Price</th><th>Runout</th></tr>`;
+	var body   = '';
+	var Eprice = 0; 
+	var Erun   = [];
 
 	for (var i = 0; i < obj['date'].length; i++) {
 
-		var row  = '';
+	  var row  = '';		
 
 		for (var k in obj) {
-			row += `<td>${obj[k][i]}</td>`;
-		}
+		  row += `<td>${obj[k][i]}</td>`;
+		  if (k == 'price')  Eprice += +obj[k][i];
+		  if (k == 'runout') Erun.push(obj[k][i]);
+	    }
 
-		body += `<tr><td>${i+1}</td>${row}</tr>`;
+	    body += `<tr><td>${i+1}</td>${row}</tr>`;
 
 	}
 
-	var t = `<table>${head}${body}</table>`;
+	var run = +Erun.pop() - +Erun.shift();
+	
+	var end = `<tr class='end'><th colspan='3'>Total</th><th>${Eprice}</th><th>${run}</th></tr>`;
+	var t   = `<table>${head}${body}${end}</table>`;
 
 	return t;
 }
@@ -74,10 +81,10 @@ function c_list_parser(obj){
 function formSubmit(e) {
 
 	var body = {
-		name : "",
-		data : ""
-	};
-	body.name = e.target.ownerDocument.activeElement.name;
+    	name : "",
+    	data : ""
+    };
+    body.name = e.target.ownerDocument.activeElement.name;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST',e.target.ownerDocument.activeElement.formAction,true);
@@ -98,9 +105,9 @@ function formSubmit(e) {
 
 	e.preventDefault();
 
-	c_handlers[e.target.ownerDocument.activeElement.name].call(formSubmit,prmtrs);
+    c_handlers[e.target.ownerDocument.activeElement.name].call(formSubmit,prmtrs);
 
-	xhr.send(JSON.stringify(body));
+    xhr.send(JSON.stringify(body));
 
 };
 
@@ -110,15 +117,15 @@ var main = document.querySelector('.main');
 
 var observer = new MutationObserver(function(mutations){
 	mutations.forEach(function(mutation){
-		if (mutation.type === 'childList'){
-			mutation.target.children[0].children.form.onsubmit = formSubmit;
-		}
+	  if (mutation.type === 'childList'){
+	  	mutation.target.children[0].children.form.onsubmit = formSubmit;
+	  }
 	})
 })
 
 observer.observe(main, {
-	attributes: true,
-	childList: true,
-	characterData: true
+  attributes: true, 
+  childList: true, 
+  characterData: true 
 });
 
